506610ca2d4c598044e3ffa3dbf9358581b616fe
Simone Ferretti <simone.ferretti.work@gmail.com>
2024-08-05 11:29:28 +0200

    chore: remove navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import NotFound from "./NotFound";
 
 export default function App() {
-  return (
-    <>
-      <Router>
-        <nav style={{ position: "fixed", top: "0" }}>
-          <Link to="/">Home</Link>
-        </nav>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Router>
-    </>
-  );
+	return (
+		<>
+			<Router>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+			</Router>
+		</>
+	);
 }
