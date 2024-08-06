1025f36e29d1b10a5d36a7bceb59058884de0b3a
Simone Ferretti <simone.ferretti.work@gmail.com>
2024-08-06 18:27:58 +0200

    feat: continue styling with theme toggle and mui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,46 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { IconButton, useMediaQuery } from "@mui/material";
+import { WbSunny } from "@mui/icons-material";
+
 import Home from "./Home";
 import NotFound from "./NotFound";
+import { useMemo, useState } from "react";
 
 export default function App() {
+	const isInitiallyDark = useMediaQuery("(prefers-color-scheme: dark)");
+
+	const [mode, setMode] = useState(isInitiallyDark ? "dark" : "light");
+
+	const switchColorMode = () => {
+		setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+	};
+
+	const theme = useMemo(
+		() =>
+			createTheme({
+				palette: {
+					mode,
+				},
+			}),
+		[mode],
+	);
+
 	return (
 		<>
-			<Router>
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="*" element={<NotFound />} />
-				</Routes>
-			</Router>
+			<IconButton color="secondary" onClick={switchColorMode}>
+				<WbSunny />
+			</IconButton>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<Router>
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="*" element={<NotFound />} />
+					</Routes>
+				</Router>
+			</ThemeProvider>
 		</>
 	);
 }
--- client/src/Projects.jsx
+++ client/src/Projects.jsx
@@ -3,35 +3,33 @@ import projectsData from "../utils/projects-data.js";
 import OpenInNewRoundedIcon from "@mui/icons-material/OpenInNewRounded";
 
 function Project({ title, url, description, technologies }) {
-  return (
-    <article className="project-article">
-      <a title={title} href={url}>
-        <div className="flex-container project-heading">
-          <h3>{title}</h3>
-          <div className="title_link-icon">
-            <OpenInNewRoundedIcon fontSize="small" />
-          </div>
-        </div>
-        <p className="project-description">{description}</p>
-      </a>
-      <div className="technologies">
-        {technologies.map((tech) => (
-          <a key={tech} title={tech.name} href={tech.url}>
-            {tech.name}
-          </a>
-        ))}
-      </div>
-    </article>
-  );
+	return (
+		<article className="project-article">
+			<a title={title} href={url}>
+				<div className="flex-container project-heading">
+					<h3>{title}</h3>
+					<OpenInNewRoundedIcon fontSize="small" />
+				</div>
+				<p className="project-description">{description}</p>
+			</a>
+			<div className="technologies">
+				{technologies.map((tech, i) => (
+					<a key={i} title={tech.name} href={tech.url}>
+						{tech.name}
+					</a>
+				))}
+			</div>
+		</article>
+	);
 }
 export default function Projects() {
-  return projectsData.map((project) => (
-    <Project
-      key={project.url}
-      title={project.title}
-      url={project.url}
-      description={project.description}
-      technologies={project.technologies}
-    />
-  ));
+	return projectsData.map((project) => (
+		<Project
+			key={project.url}
+			title={project.title}
+			url={project.url}
+			description={project.description}
+			technologies={project.technologies}
+		/>
+	));
 }
