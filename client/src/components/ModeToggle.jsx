11b631ecfdad0ea435740997e9fb2f5418b6b6fb
Simone Ferretti <simone.ferretti.work@gmail.com>
2024-08-13 15:23:04 +0200

    feat: edit theme changing button to support 3 states

--- client/src/components/ModeToggle.jsx
+++ client/src/components/ModeToggle.jsx
@@ -1,25 +1,43 @@
-import { useState, useEffect } from "react";
-import { useColorScheme, Button } from "@mui/joy";
+import { useColorScheme } from "@mui/joy/styles";
+import { Chip } from "@mui/joy";
+import { LightMode, ModeNight, Adjust } from "@mui/icons-material";
+import { useState } from "react";
 
 export default function ModeToggle() {
 	const { mode, setMode } = useColorScheme();
-	const [mounted, setMounted] = useState(false);
+	const [currentState, setCurrentState] = useState("system");
 
-	useEffect(() => {
-		setMounted(true);
-	}, []);
-	if (!mounted) {
-		return null;
+	const states = {
+		light: {
+			name: "Light theme",
+			icon: <LightMode />,
+		},
+		dark: {
+			name: "Dark theme",
+			icon: <ModeNight />,
+		},
+		system: {
+			name: "System theme",
+			icon: <Adjust />,
+		},
+	};
+
+	function changeState() {
+		if (currentState === "dark") setCurrentState("system");
+		else if (currentState === "system") setCurrentState("light");
+		else if (currentState === "light") setCurrentState("dark");
+		console.log(currentState, mode);
+		setMode(currentState);
 	}
 
 	return (
-		<Button
-			variant="soft"
-			onClick={() => {
-				setMode(mode === "light" ? "dark" : "light");
-			}}
+		<Chip
+			variant="outlined"
+			startDecorator={states[mode].icon}
+			color="neutral"
+			onClick={changeState}
 		>
-			{mode === "light" ? "Turn dark" : "Turn light"}
-		</Button>
+			{states[mode].name}
+		</Chip>
 	);
 }
