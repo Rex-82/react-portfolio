ac33fcc10d1a7add9f1122bcff3deedff1995422
Simone Ferretti <simone.ferretti.work@gmail.com>
2024-08-14 00:17:54 +0200

    feat: move Header and Footer to App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,22 +4,29 @@ import { CssVarsProvider } from "@mui/joy/styles";
 
 import Home from "pages/Home";
 import NotFound from "pages/NotFound";
+import Dotfiles from "pages/Dotfiles";
 // import ThemeTypography from "./components/storybooks/ThemeTypography";
 
-import ModeToggle from "components/ModeToggle";
+import Header from "components/Header";
+import Footer from "components/Footer";
 import CustomTheme from "utils/CustomTheme";
 
 export default function App() {
 	return (
-		<CssVarsProvider defaultMode="system" theme={CustomTheme}>
-			<CssBaseline />
-			<Router>
-				<Routes>
-					<Route path="/" element={<Home />} />
-					{/* <Route path="/typography" element={<ThemeTypography />} /> */}
-					<Route path="*" element={<NotFound />} />
-				</Routes>
-			</Router>
-		</CssVarsProvider>
+		<>
+			<CssVarsProvider defaultMode="system" theme={CustomTheme}>
+				<CssBaseline />
+				<Header />
+				<Router>
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/dotfiles" element={<Dotfiles />} />
+						{/* <Route path="/typography" element={<ThemeTypography />} /> */}
+						<Route path="*" element={<NotFound />} />
+					</Routes>
+				</Router>
+				<Footer />
+			</CssVarsProvider>
+		</>
 	);
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,15 +1,11 @@
-import Header from "components/Header";
 import Projects from "components/Projects";
-import Footer from "components/Footer";
 import { Typography } from "@mui/joy";
 
 export default function Home() {
 	return (
 		<>
-			<Header />
 			<Typography level="title1">Simone Ferretti</Typography>
 			<Projects />
-			<Footer />
 		</>
 	);
 }
