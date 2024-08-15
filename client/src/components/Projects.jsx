fae9f4b0387cc82a2b59c68c62ba2c29d2dc15e4
Simone Ferretti <simone.ferretti.work@gmail.com>
2024-08-15 18:22:05 +0200

    feat: improve query logic to handle cache

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -1,5 +1,5 @@
-import { Card, Link, Typography, Chip, Stack, Box } from "@mui/joy";
-import queryData from "utils/QueryData";
+import { Card, Link, Typography, Chip, Stack } from "@mui/joy";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 
 function Project({ title, url, description, topics }) {
 	return (
@@ -25,34 +25,57 @@ function Project({ title, url, description, topics }) {
 		</Card>
 	);
 }
+
 export default function Projects() {
-	const endpoint = "https://api.github.com/users/Rex-82/repos";
-	const queryKey = ["reposData"];
+	const queryClient = useQueryClient();
 
-	const { status, data, error } = queryData(queryKey, endpoint);
-
-	if (status === "pending") {
-		return "Loading...";
-	}
-	if (status === "error") {
-		return `An error has occured: ${error.message}`;
+	async function fetchRepoData() {
+		const response = await fetch("https://api.github.com/users/Rex-82/repos");
+		if (!response.ok) {
+			throw new Error(response);
+		}
+		return response.json();
 	}
 
+	// Check if the data is already cached
+	let data = queryClient.getQueryData("repoData");
+
+	// If data is not cached, use the query hook to fetch it
+	if (!data) {
+		const {
+			status,
+			data: queryData,
+			error,
+		} = useQuery({
+			queryKey: ["repoData"],
+			queryFn: fetchRepoData,
+			staleTime: 1000 * 60 * 60, // 1 hour in milliseconds
+		});
+
+		if (status === "loading") {
+			return "Loading...";
+		}
+		if (status === "error") {
+			return `An error has occurred: ${error.message}`;
+		}
+
+		data = queryData;
+	}
+
+	// Filter and map over the data to render the projects
 	return (
 		<Stack direction="column" spacing={2}>
-			{data.map((repo) => {
-				if (!repo.fork && repo.description !== null) {
-					return (
-						<Project
-							key={repo.id}
-							title={repo.name}
-							url={repo.html_url}
-							description={repo.description}
-							topics={repo.topics}
-						/>
-					);
-				}
-			})}
+			{data
+				?.filter((repo) => !repo.fork && repo.description !== null)
+				.map((repo) => (
+					<Project
+						key={repo.id}
+						title={repo.name}
+						url={repo.html_url}
+						description={repo.description}
+						topics={repo.topics}
+					/>
+				))}
 		</Stack>
 	);
 }
--- client/src/pages/Dotfiles.jsx
+++ client/src/pages/Dotfiles.jsx
@@ -1,23 +1,35 @@
-import { useEffect, useState } from "react";
-
-import { useTheme } from "@mui/joy";
-import { Typography, CardOverflow, Stack, Chip } from "@mui/joy";
-
-import { Light as SyntaxHighlighter } from "react-syntax-highlighter";
-import shell from "react-syntax-highlighter/dist/esm/languages/prism/shell-session";
+import {
+	Typography,
+	Stack,
+	IconButton,
+	Tooltip,
+	ButtonGroup,
+	CardOverflow,
+} from "@mui/joy";
+import { OpenInNew, FileCopy } from "@mui/icons-material";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import SyntaxHighlighter from "react-syntax-highlighter";
 import {
 	gruvboxDark,
 	gruvboxLight,
 } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import shell from "react-syntax-highlighter/dist/esm/languages/prism/shell-session";
+import { useTheme } from "@emotion/react";
+import { useEffect, useState, memo } from "react";
 
-export default function Dotfiles() {
-	const [data, setData] = useState([]);
-	const [loading, setLoading] = useState(true);
-	const [error, setError] = useState(null);
-
+const Dotfiles = memo(function Dotfiles() {
 	const theme = useTheme();
+	const queryClient = useQueryClient();
 
-	const url = "https://api.github.com";
+	// State to control when to enable the query
+	const [isQueryEnabled, setIsQueryEnabled] = useState(false);
+
+	// useEffect to enable the query on the first render
+	useEffect(() => {
+		setIsQueryEnabled(true);
+	}, []);
+
 	const repo = "Rex-82/dotfiles";
 	const branch = "main";
 
@@ -27,83 +39,113 @@ export default function Dotfiles() {
 		{ name: ".zshrc", path: ".config/zsh/.zshrc" },
 	];
 
-	useEffect(() => {
-		async function fetchData() {
-			try {
-				const urls = files.map(
-					(file) => `${url}/repos/${repo}/contents/${file.path}?ref=${branch}`,
-				);
+	const urls = files.map(
+		(file) =>
+			`https://api.github.com/repos/${repo}/contents/${file.path}?ref=${branch}`,
+	);
 
-				const responses = await Promise.all(
-					urls.map(async (url) => {
-						const header = new Headers();
-						header.append("Accept", "application/vnd.github.VERSION.raw");
+	// Function to fetch file content
+	const fetchFileContent = async (url) => {
+		const response = await fetch(url, {
+			headers: {
+				Accept: "application/vnd.github.VERSION.raw",
+			},
+		});
+		if (!response.ok) {
+			throw new Error(`Failed to fetch: ${response.statusText}`);
+		}
+		return response.text();
+	};
 
-						const options = {
-							headers: header,
-						};
+	// Check if data is already in cache
+	let data = queryClient.getQueryData("dotfilesData");
 
-						const response = await fetch(url, options);
-						if (!response.ok) {
-							throw new Error(`Failed to fetch: ${response.statusText}`);
-						}
+	if (!data) {
+		// Use useQuery to fetch the data if it's not in cache
+		const {
+			status,
+			data: queryData,
+			error,
+		} = useQuery({
+			queryKey: ["dotfilesData"],
+			queryFn: () => Promise.all(urls.map(fetchFileContent)),
+			staleTime: 1000 * 60 * 60, // 1 hour in milliseconds
+			enabled: isQueryEnabled, // Enable the query based on state
+		});
 
-						return await response.text();
-					}),
-				);
-
-				setData(responses);
-				setLoading(false);
-			} catch (err) {
-				setError(err);
-				setLoading(false);
-			}
+		if (status === "pending" || status === "loading") {
+			return "Loading...";
+		}
+		if (status === "error") {
+			return `An error has occurred: ${error.message}`;
 		}
 
-		fetchData();
-	}, []);
-
-	if (loading) {
-		return <Typography>Loading...</Typography>;
+		// Save the fetched data to the variable `data`
+		data = queryData;
 	}
-
-	if (error) {
-		return <Typography>Error: {error.message}</Typography>;
-	}
-
 	return (
 		<>
 			<Typography level="title1">Dotfiles</Typography>
-			<Stack
-				direction="column"
-				justifyContent="center"
-				alignItems="stretch"
-				spacing={2}
-			>
+			<Stack direction="column" spacing={2}>
 				{data.map((d, i) => (
-					<div key={i}>
-						<Typography level="callout">{files[i].name}</Typography>
-
-						<CardOverflow value={i} sx={{ justifyContent: "center" }}>
-							<SyntaxHighlighter
-								showLineNumbers
-								clip
-								language={shell}
-								style={
-									theme.palette.mode === "dark" ? gruvboxDark : gruvboxLight
-								}
-								customStyle={{
-									fontSize: theme.fontSize.sm,
-									maxWidth: "95%",
-									borderRadius: theme.radius.md,
-								}}
+					<CardOverflow
+						key={i}
+						sx={{
+							flexDirection: "column",
+							alignSelf: "center",
+							maxWidth: "95%",
+						}}
+					>
+						<Stack
+							direction="row"
+							alignItems="center"
+							width="100%"
+							sx={(theme) => ({
+								borderTopRightRadius: [theme.radius.md],
+								borderTopLeftRadius: [theme.radius.md],
+								backgroundColor: [theme.palette.background.level2],
+								padding: "0 0.5em",
+							})}
+							justifyContent="space-between"
+						>
+							<Typography level="callout">{files[i].name}</Typography>
+							<ButtonGroup
+								size="sm"
+								sx={(theme) => ({
+									borderRadius: [theme.radius.lg],
+								})}
 							>
-								{d}
-							</SyntaxHighlighter>
-						</CardOverflow>
-					</div>
+								<Tooltip arrow title="Go to github repository">
+									<IconButton>
+										<OpenInNew />
+									</IconButton>
+								</Tooltip>
+								<CopyToClipboard text={d}>
+									<IconButton>
+										<FileCopy />
+									</IconButton>
+								</CopyToClipboard>
+							</ButtonGroup>
+						</Stack>
+						<SyntaxHighlighter
+							showLineNumbers
+							language={shell}
+							style={theme.palette.mode === "dark" ? gruvboxDark : gruvboxLight}
+							customStyle={{
+								fontSize: [theme.fontSize.sm],
+								borderBottomLeftRadius: [theme.radius.md],
+								borderBottomRightRadius: [theme.radius.md],
+								width: "100%",
+								margin: 0,
+							}}
+						>
+							{d}
+						</SyntaxHighlighter>
+					</CardOverflow>
 				))}
 			</Stack>
 		</>
 	);
-}
+});
+
+export default Dotfiles;
