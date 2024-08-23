e8e1e6989e5710c56a861c839153b972e309818a
Simone Ferretti <simone.ferretti.work@gmail.com>
2024-08-23 22:53:55 +0200

    feat: wrap pages content inside box component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { CssBaseline } from "@mui/joy";
+import { Box, CssBaseline } from "@mui/joy";
 import { CssVarsProvider } from "@mui/joy/styles";
 import InitColorSchemeScript from "@mui/joy/InitColorSchemeScript";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -28,48 +28,50 @@ export default function App() {
 					<CssBaseline />
 					<Router>
 						<Header />
-						<Routes>
-							<Route
-								path="/"
-								element={
-									<Home>
-										<PageDesc
-											title="Projects"
-											description="Here are some projects I worked on"
-										/>
-									</Home>
-								}
-							/>
-							<Route
-								path="/dotfiles"
-								element={
-									<Suspense
-										fallback={
-											<>
+						<Box padding="0.5rem">
+							<Routes>
+								<Route
+									path="/"
+									element={
+										<Home>
+											<PageDesc
+												title="Projects"
+												description="Here are some projects I worked on"
+											/>
+										</Home>
+									}
+								/>
+								<Route
+									path="/dotfiles"
+									element={
+										<Suspense
+											fallback={
+												<>
+													<PageDesc
+														title="Dotfiles"
+														description="Below you can see the files i use in my linux system"
+													/>
+													<DotfileSkeleton />
+													<DotfileSkeleton />
+													<DotfileSkeleton />
+												</>
+											}
+										>
+											<Dotfiles>
 												<PageDesc
 													title="Dotfiles"
 													description="Below you can see the files i use in my linux system"
 												/>
-												<DotfileSkeleton />
-												<DotfileSkeleton />
-												<DotfileSkeleton />
-											</>
-										}
-									>
-										<Dotfiles>
-											<PageDesc
-												title="Dotfiles"
-												description="Below you can see the files i use in my linux system"
-											/>
-										</Dotfiles>
-									</Suspense>
-								}
-							/>
-							{/* <Route path="/typography" element={<ThemeTypography />} /> */}
-							<Route path="*" element={<NotFound />} />
-						</Routes>
+											</Dotfiles>
+										</Suspense>
+									}
+								/>
+								{/* <Route path="/typography" element={<ThemeTypography />} /> */}
+								<Route path="*" element={<NotFound />} />
+							</Routes>
+						</Box>
+						<Footer />
 					</Router>
-					<Footer />
 				</CssVarsProvider>
 			</QueryClientProvider>
 		</>
